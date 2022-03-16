39ab7ebce51f7a8a9ff59de973cce5ba45548183
L II <lii@Ls-MacBook-Pro.lan>
2022-03-16 04:46:45 -0500

    updated Header.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
 import './App.css';
 import Header from './components/Header';
+import SearchIcon from '@mui/icons-material/Search';
+import Country from './components/Country';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className='app-body'>
+        <div className='inputs'>
+          <div className='search-input'>
+            <SearchIcon />
+            <input type='text' placeholder='Search for a country...' />
+          </div>
+          <div className='select-region'>
+            <select>
+              <option>All</option>
+              <option>Africa</option>
+              <option>Americas</option>
+              <option>Asia</option>
+              <option>Europe</option>
+              <option>Oceania</option>
+              
+            </select>
+          </div>
+        </div>
+        <div className='countries'>
+          <Country />
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,17 @@ import DarkModeIcon from '@mui/icons-material/DarkMode'
 function Header() {
   return (
     <div className='header'>
-        <div className='header_container'></div>
-        <h2 className='logo'>Where in the world?</h2>
-        <div className='switch_mode'></div>
-        <DarkModeIcon />
-        <h3>Dark Mode</h3>
-    
+        <div className='header-container'>
+            <h2 className='logo'>Where in the world?</h2>
+            <div className='switch-mode'>
+                <DarkModeIcon />
+                <h3>Dark Mode</h3>
+            </div>
+        </div>
     </div>
+
+        
+    
   )
 }
 
